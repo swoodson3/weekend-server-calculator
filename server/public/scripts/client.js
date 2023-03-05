b64902b717987b016901b56826a24b7259064712
Seth Woodson <sethwoodson3@gmail.com>
2023-03-05 13:06:41 -0600

    client-js

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,19 +1,121 @@
 console.log('script sourced.');
 
-function calculate() {
-    const num1 = document.getElementById('num1').value;
-    const num2 = document.getElementById('num2').value;
-    const operator = document.getElementById('operator').value;
-    const data ={ num1, num2, operator};
+let finalVal; 
 
-    axios.post('/calculate', data).then((response) => {
+function operatorFunction(event) {
+    finalVal = event.target.value; 
+    console.log(finalVal)
+}
+
+function calculate(event) {
+    let value1 = document.querySelector('#value1').value;
+    let value2 = document.querySelector('#value2').value;
+    let operation = finalVal;
+    console.log(value1, value2, operation);
+
+    let values = {
+        first: value1,
+        second: value2,
+        operator: operation
+    };
+    console.log(values);
+    axios.post('/numbers', values).then((response) => {
         console.log(response);
-        addToHistory();
-        clearInput();
+        getData();
+        addToHistory()
+    }).catch((error) => {
+        console.log(error);
+        alert('Something went wrong!')
+    });
+}
+
+function getData() {
+    axios.get('/numbers').then((response) => {
+        console.log(response)
+        let dataFromServer = response.data
+        let contentTable = document.querySelector('#history');
+        contentTable.innerHTML = '';
+        for (let data of dataFromServer) {
+            contentTable.innerHTML += `
+            <h2>${data}</h2>
+            `;
+        }
     }).catch((error) => {
         console.log(error);
         alert('Something went wrong')
-    });
-    } 
+    })
+};
 
-    
+getData();
+
+function addToHistory() {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*// send a POST request to the server with the input data
+function calculate() {
+    event.preventDefault();
+    const value1 = document.getElementById("#value1").value;
+    const value2 = document.getElementById("#value2").value;
+    const operation = document.getElementById("operation").value;
+    console.log(calculate);
+
+    const data = { value1, value2, operation };
+    axios.post("/calculate", data).then((response) => {
+        console.log(response)
+            getHistory();
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+}
+
+// send a GET request to the server to get the historical data
+function getHistory() {
+    axios.get("/history").then((response) => {
+            const history = response.data;
+            const historyList = document.getElementById("history");
+            historyList.innerHTML = "";
+            history.forEach((item) => {
+                const li = document.createElement("li");
+                li.innerText = `${item.value1} ${item.operation} ${item.value2} = ${item.result}`;
+                historyList.appendChild(li);
+            });
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+}
+
+// clear the input fields
+function clearInput() {
+    document.getElementById("value1").value = "";
+    document.getElementById("value2").value = "";
+}
+
+// get the historical data when the page loads
+getHistory();*/
--- server/server.js
+++ server/server.js
@@ -2,28 +2,48 @@
 const express = require('express')
 
 
-
 const app = express();
 // Heroku assigns us a unique PORT
 // Use 5001 for localhost development 
 const port = process.env.PORT || 5001;
 
-let history = [];
+const history = [];
 
+app.post("/calculate", (req, res) => {
+    console.log('POST Request made for /calculate')
+    const { value1, value2, operation } = req.body;
+    let result;
 
-app.get('/history', (req, res) => {
-	res.status(200).send(history);
+    switch (operation) {
+        case "add":
+            result = Number(value1) + Number(value2);
+            break;
+        case "subtract":
+            result = Number(value1) - Number(value2);
+            break;
+        case "multiply":
+            result = Number(value1) * Number(value2);
+            break;
+        case "divide":
+            result = Number(value1) / Number(value2);
+            break;
+        default:
+            result = "Invalid operation";
+    }
+
+    // add the calculation to the history array
+    const calculation = { value1, value2, operation, result };
+    history.push(calculation);
+
+    res.sendStatus(200);
 });
 
-app.post('/calculate', (req, res) => {
-    console.log('POST Request made for /calculate');
-    // Any data we send from the client is available
-    // as a property of req.body
-    console.log(req.body);
-    let numberToAdd = req.body
-    history.push(numberToAdd);
-    res.sendStatus(201); // Success!
+// handle GET requests to the /history endpoint
+app.get("/history", (req, res) => {
+    console.log('GET request made for /history')
+    res.send(history);
 });
+
 
 // Look here for files
 app.use(express.json());
